5abaea719c6d0f0147211f3c5ccbc649623e8b28
weapon-x <julianperge@gmail.com>
2018-06-18 10:57:24 -0400

    initial commit

---
+++ src/main/java/com/julian/virtualpetspring/VirtualPetSpringApplication.java
@@ -0,0 +1,12 @@
+package com.julian.virtualpetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPetSpringApplication.class, args);
+	}
+}
